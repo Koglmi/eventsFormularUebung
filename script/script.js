62faef3c425a85032ca41cd2156d6bac9c0c7174
Koglmi <40146720180269@litec.ac.at>
2020-01-04 13:32:44 +0100

    checkbox konrolle

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', () => {
 
     const sub = document.getElementById('sub');
+    
 
-
+    
     sub.addEventListener('click', () => {
         const vorname = document.getElementById('firstname');
         const nachname =document.getElementById('surname');
@@ -33,7 +34,7 @@ window.addEventListener('load', () => {
         }
 
         if(straße.value === '') {
-            straßecontroll = 'Der Straße wurde nicht eingegeben! ';
+            straßecontroll = 'Die Straße wurde nicht eingegeben! ';
         }
         else{
             straßecontroll = '';
@@ -53,20 +54,76 @@ window.addEventListener('load', () => {
             emailcontroll = '';
         }
 
+    
+        
+
 
         // alert(vornamecontroll + nachnamecontroll + straßecontroll + plzcontroll + emailcontroll);
 
 
         info.textContent = vornamecontroll + nachnamecontroll + straßecontroll + plzcontroll + emailcontroll;
 
+        if(check === true) {
+            hideAll();
+            changeDisplayProperty('teilnahme', 'block');
+        }
+
+        
     })
 
     document.getElementById('preis').addEventListener('input', ausgeben);
+
+    
         
 })
+
+let check = false;
 
 function ausgeben(ev) {
     document.getElementById('preisanzeige').value = ev.target.value;
 }
+
+function checkbox() {
+    let urlaubsziele = document.getElementsByClassName('urlaubsziele');
+
+    if(urlaubsziele[0].checked) {
+        check = true;
+    }
+
+    if(urlaubsziele[1].checked) {
+        check = true;
+    }
+
+    if(urlaubsziele[2].checked) {
+        check = true;
+    }
+
+    if(urlaubsziele[3].checked) {
+        check = true;
+    }
+
+    if(check === false) {       
+    }
+
+}
+
+function changeDisplayProperty(id, property) {
+    document.getElementById(id).style.display = property;
+}
+
+function hideAll() {
+
+    const elements = [document.getElementById('content'),
+                      document.getElementById('teilnahme'),
+                    ];
+  
+    for (const element of elements) {
+      element.style.display = 'none';
+    }
+  }
+
+
+    
+
 
 
